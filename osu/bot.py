fe00e8e20fc7591dba9a740acbf2a9fdb95b5dee
OStubbs <45260598+OStubbs@users.noreply.github.com>
2018-12-06 20:50:41 +0000

    Added actual osu command

--- osu/bot.py
+++ osu/bot.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 import requests
 client = discord.Client()
-
+osuApiKey = ""#put you osu!api key here
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
@@ -15,5 +15,18 @@ async def on_message(message):
     if message.content.startswith("!help"):
         msgEmbed = discord.Embed(title="Help", description="Commands")
         await message.channel.send(embed=msgEmbed)
-
+    
+        
+@client.command(pass_context=True)
+async def osu(ctx, username:str):
+    parameters = dict(k=osuApiKey, u=username)
+    response = requests.get(url="https://osu.ppy.sh/api/get_user", params=parameters)
+    data = response.json()
+    profile = ""
+    profile += "Username: {}\n".format(data["username"])
+    profile += "Raw PP: {}\n".format(data["pp_raw"])
+    profile += "Accuracy: {}\n".format(round(data["accuracy"],2))#Round to 2 d.p
+    profile += "Country: {}".format(data["country"])
+    await ctx.send(profile)#Change this to a embed, looks cleaner.
+    
 client.run('')#Put bot token here
